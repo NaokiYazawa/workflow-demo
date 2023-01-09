e7fb72ca1b5f05477384f74e23c3553336458eff
NaokiYazawa <yazawa.naoki.01@gmail.com>
2023-01-09 09:46:47 +0900

    init

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/NaokiYazawa/go-simple-server/server"
+)
+
+const (
+	host = "0.0.0.0"
+	port = "8080"
+)
+
+func main() {
+	s := server.New()
+	err := s.Start(fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		log.Fatalf("server stopped with error: %s", err)
+	}
+
+	log.Printf("server stopped successfully")
+}
